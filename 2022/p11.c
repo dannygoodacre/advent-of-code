7dd1230166bbab1d9308e24777751634c5b58de8
Danny Goodacre <dannygoodacre1@gmail.com>
2023-01-16 17:33:51 +0000

    Add stability, memory, and structural improvements.

--- 2022/p11.c
+++ 2022/p11.c
@@ -9,21 +9,19 @@ int compare(const void *a, const void *b)
 
 typedef struct Queue
 {
-    int start;
-    int end;
-    int n;
-    size_t items[1000000];
+    int start, end, n;
+    size_t items[1000000]; // This could be made more efficient.
 } Queue;
 
 typedef struct Monkey
 {
-    Queue *items;
-    char op;
-    char opn[10];
-    int testn;
-    int monkeytrue;
-    int monkeyfalse;
-    int ninspects;
+    Queue *items; 
+    char op; // Operator for changing worry level.
+    char opn[10]; // Operand for changing worry level.
+    int testn; // Number used for testing.
+    int monkeytrue; // Throw to if test true.
+    int monkeyfalse; // Throw to if test false.
+    int ninspects; // No. of inspects performed.
 } Monkey;
 
 void push(Queue *queue, size_t item)
@@ -34,8 +32,6 @@ void push(Queue *queue, size_t item)
 
 int pop(Queue *queue)
 {
-    // if (!queue->n)
-    //     return -1; // All values for our purposes are non-negative.
     queue->n--;
     return queue->items[queue->start++];
 }
@@ -98,50 +94,57 @@ void taketurn(Monkey *monkeys, int monkeyn, int n, size_t s)
     }
 }
 
-int main()
+// Fill in array of monkeys with data from input file.
+void getdata(Monkey *monkeys, int *monkeyn, size_t *s)
 {
     FILE *f = fopen("input.txt", "r");
-    char *line, op, opn[10];
-    size_t len, s = 1;
-    int monkeyn;
-    Monkey monkeys[10];
-    for (int i = 0; i < 10; i++)
-        monkeys[i].items = malloc(sizeof(Queue));
-
-    // Part 2
+    char *line = NULL, op, opn[10];
+    size_t len;
     for (int i = 0; getline(&line, &len, f) != -1; i++)
     {
         switch (i % 7)
         {
             case 0: // Monkey.
-                sscanf(line, "Monkey %d:\n", &monkeyn);
-                monkeys[monkeyn].items->start = 0;
-                monkeys[monkeyn].items->end = -1;
-                monkeys[monkeyn].items->n = 0;
-                monkeys[monkeyn].ninspects = 0;
+                sscanf(line, "Monkey %d:\n", monkeyn);
+                monkeys[*monkeyn].items->start = 0;
+                monkeys[*monkeyn].items->end = -1;
+                monkeys[*monkeyn].items->n = 0;
+                monkeys[*monkeyn].ninspects = 0;
                 break;
             case 1: // Starting items.
-                getitems(monkeys[monkeyn], strchr(line, ':') + 2);
+                getitems(monkeys[*monkeyn], strchr(line, ':') + 2);
                 break;
             case 2: // Operation.
                 sscanf(strchr(line, 'd') + 2, "%c %s\n", &op, opn);
-                monkeys[monkeyn].op = op;
-                strcpy(monkeys[monkeyn].opn, opn);
+                monkeys[*monkeyn].op = op;
+                strcpy(monkeys[*monkeyn].opn, opn);
                 break;
             case 3: // Divisibility test.
-                monkeys[monkeyn].testn = atoi(strchr(line, 'y') + 2);
-                s *= monkeys[monkeyn].testn;
+                monkeys[*monkeyn].testn = atoi(strchr(line, 'y') + 2);
+                *s *= monkeys[*monkeyn].testn;
                 break;
             case 4: // Monkey if true.
-                monkeys[monkeyn].monkeytrue = atoi(strchr(line, 'y') + 2);
+                monkeys[*monkeyn].monkeytrue = atoi(strchr(line, 'y') + 2);
                 break;
             case 5: // Monkey if false.
-                monkeys[monkeyn].monkeyfalse = atoi(strchr(line, 'y') + 2);
+                monkeys[*monkeyn].monkeyfalse = atoi(strchr(line, 'y') + 2);
         }
     }
-    monkeyn++; // Total no. of monkeys.
+    (*monkeyn)++;
+    fclose(f);
+    free(line);
+}
 
-    for (int i = 0; i < 10000; i++)
+// Return level of monkey business after simulating n rounds.
+size_t run(int n, size_t s)
+{
+    int monkeyn; // Total no. of monkeys.
+    Monkey monkeys[10];
+    for (int i = 0; i < 10; i++)
+        monkeys[i].items = malloc(sizeof(Queue));
+
+    getdata(&monkeys[0], &monkeyn, &s);
+    for (int i = 0; i < n; i++)
         for (int j = 0; j < monkeyn; j++)
             taketurn(&monkeys[0], monkeyn, j, s);
 
@@ -149,59 +152,16 @@ int main()
     for (int i = 0; i < monkeyn; i++)
         inspects[i] = monkeys[i].ninspects;
     qsort(inspects, monkeyn, sizeof(size_t), compare);
-    printf("2: %lu\n", inspects[0] * inspects[1]);
-
-    // Part 1
-    rewind(f);
-    monkeyn = 0;
-    for (int i = 0; i < 10; i++)
-        monkeys[i].items = malloc(sizeof(Queue));
-    // Get input data.
-    for (int i = 0; getline(&line, &len, f) != -1; i++)
-    {
-        switch (i % 7)
-        {
-            case 0: // Monkey.
-                sscanf(line, "Monkey %d:\n", &monkeyn);
-                monkeys[monkeyn].items->start = 0;
-                monkeys[monkeyn].items->end = -1;
-                monkeys[monkeyn].items->n = 0;
-                monkeys[monkeyn].ninspects = 0;
-                break;
-            case 1: // Starting items.
-                getitems(monkeys[monkeyn], strchr(line, ':') + 2);
-                break;
-            case 2: // Operation.
-                sscanf(strchr(line, 'd') + 2, "%c %s\n", &op, opn);
-                monkeys[monkeyn].op = op;
-                strcpy(monkeys[monkeyn].opn, opn);
-                break;
-            case 3: // Divisibility test.
-                monkeys[monkeyn].testn = atoi(strchr(line, 'y') + 2);
-                s *= monkeys[monkeyn].testn;
-                break;
-            case 4: // Monkey if true.
-                monkeys[monkeyn].monkeytrue = atoi(strchr(line, 'y') + 2);
-                break;
-            case 5: // Monkey if false.
-                monkeys[monkeyn].monkeyfalse = atoi(strchr(line, 'y') + 2);
-        }
-    }
-    fclose(f);
-    free(line);
-    monkeyn++; // Total no. of monkeys.
-
-    for (int i = 0; i < 20; i++)
-        for (int j = 0; j < monkeyn; j++)
-            taketurn(&monkeys[0], monkeyn, j, 0);
-
-    for (int i = 0; i < monkeyn; i++)
-        inspects[i] = monkeys[i].ninspects;
-    qsort(inspects, monkeyn, sizeof(size_t), compare);
-    printf("1: %lu\n", inspects[0] * inspects[1]);
+    size_t out = inspects[0] * inspects[1];
 
     for (int i = 0; i < 10; i++)
         free(monkeys[i].items);
     free(inspects);
+    return out;
+}
+
+int main()
+{
+    printf("1: %lu\n2: %lu\n", run(20, 0), run(10000, 1));
     return 0;
 }
